7c72c18b5ff0f6cf71c603396e06632a12aca54e
deme <joaommdemenech@gmail.com>
2023-03-02 08:40:07 -0300

    [#3,drd,flowershow plugins][xs]: remove URL parameter from view.js

--- pages/tools/view.js
+++ pages/tools/view.js
@@ -1,23 +1,27 @@
-import parse from "../../lib/markdown.js";
-import { getContentFromUrl } from "lib/remoteFiles";
+import fs from 'fs'
+import path from 'path'
 
-import DRD from "../../components/drd/DRD";
+import parse from '../../lib/markdown.js'
+
+import DRD from '../../components/drd/DRD'
+
 
 export default function DRDPage({ source, frontMatter }) {
-  return <DRD source={source} frontMatter={frontMatter} />;
+  return (
+    <DRD source={source} frontMatter={frontMatter} />
+  )
 }
 
-export const getServerSideProps = async ({ query, res }) => {
-  const { url } = query;
+export const getStaticProps = async ({ params }) => {
+  const mdxPath = path.join('content', 'drd', 'demo.mdx')
+  const source = fs.readFileSync(mdxPath)
 
-  const fileContent = await getContentFromUrl(url);
-
-  const { mdxSource, frontMatter } = await parse(fileContent, url);
+  const { mdxSource, frontMatter } = await parse(source, 'demo.mdx')
 
   return {
     props: {
       source: mdxSource,
       frontMatter: frontMatter,
     },
-  };
-};
+  }
+}
