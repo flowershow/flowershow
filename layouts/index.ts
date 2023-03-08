b710bb1a95283f42584018334c1354bd8fcd29fa
olayway <ola@yway.pl>
2023-03-08 10:59:02 +0100

    upgrade layouts

---
+++ layouts/index.ts
@@ -0,0 +1,16 @@
+import {
+  SimpleLayout,
+  DocsLayout,
+  UnstyledLayout,
+  BlogLayout,
+} from "@flowershow/core";
+
+import CollectionsLayout from "./collection";
+
+export default {
+  simple: SimpleLayout,
+  docs: DocsLayout,
+  unstyled: UnstyledLayout,
+  blog: BlogLayout,
+  collection: CollectionsLayout
+};
