5993640e151b492fcf327df0522c071c4bc68f86
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-06-02 14:06:40 -0300

    [banner][xs] - change banner text

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -17,7 +17,7 @@ export default function Banner() {
       <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-zinc-900 text-white px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
         <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
           <p className="text-sm leading-6 text-white">
-            Discover 🌀 PortalJS - The JavaScript framework for Data Portals
+            Discover 🌀 PortalJS - the JavaScript framework for data portals.
           </p>
           <a
             href="https://portaljs.org"
