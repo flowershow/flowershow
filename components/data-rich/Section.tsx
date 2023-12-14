7df1d3bf4024618b0dcfc5ec1cbddd4a11c4417a
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-12-14 14:57:59 +0100

    [home][xs]: add basic layout to home pages

--- components/data-rich/Section.tsx
+++ components/data-rich/Section.tsx
@@ -8,7 +8,7 @@ export function Section({
 }: React.ComponentPropsWithoutRef<'section'>) {
     return (
         <section
-            className={clsx("mx-auto max-w-2xl px-6 lg:max-w-none mt-24 md:mt-36", className)}
+            className={clsx("mt-24 md:mt-36", className)}
             {...props}
         >
             {children}
