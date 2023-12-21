b4e3427a5a49e5ec8b5e6236a81daf0fe7740b54
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-12-21 17:07:37 +0100

    [dararich landing page][xs]: escape character

--- components/data-rich/About.tsx
+++ components/data-rich/About.tsx
@@ -15,7 +15,7 @@ export function About() {
                 </strong>
                 to make data storytelling and analysis easy.
                 And so, in data-rich document the writer can easily mix formatted text content with data visualisations.
-                This means that you don't have to code or embed your charts and tables; they can be added to the document with a very simple syntax, either by passing inline data or simply referencing your data files.
+                This means that you don&apos;t have to code or embed your charts and tables; they can be added to the document with a very simple syntax, either by passing inline data or simply referencing your data files.
                 What you end up with is a
                 <strong>
                     {" "}plain text, human-readable document enriched with data visualisations
