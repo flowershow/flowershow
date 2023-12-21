9e17f71f9828c77f91334d6f8691ed62e9eafdd1
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-12-21 17:50:17 +0100

    [site domains settings][xs]: fix form inputs stacking on top of sidebar

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -61,7 +61,7 @@ export default function Form({
                     }
                 });
             }}
-            className="rounded-lg border border-stone-200 bg-white dark:border-stone-700 dark:bg-black"
+            className="isolate rounded-lg border border-stone-200 bg-white dark:border-stone-700 dark:bg-black"
         >
             <div className="relative flex flex-col space-y-4 p-5 sm:p-10">
                 <h2 className="font-cal text-xl dark:text-white">{title}</h2>
