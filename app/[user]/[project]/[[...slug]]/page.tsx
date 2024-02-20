b77ec9fb44e295278ceeeade2f0fc80151c91dca
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-02-20 11:51:11 +0100

    [user/project/slug,#169][xs]: comment out ISR as causing error
    - error: Dynamic server usage: Page couldn't be rendered statically because it used `cookies`

--- app/[user]/[project]/[[...slug]]/page.tsx
+++ app/[user]/[project]/[[...slug]]/page.tsx
@@ -43,69 +43,69 @@ import parse from "@/lib/markdown";
  *     };
  * } */
 
-export async function generateStaticParams() {
-  // retrun any static params here,
-  // e.g. all user sites index pages, or all pages for premium users
-  return [];
-}
+/* export async function generateStaticParams() {
+*   // retrun any static params here,
+*   // e.g. all user sites index pages, or all pages for premium users
+*   return [];
+* } */
 
 export default async function SitePage({
-  params,
+    params,
 }: {
-  params: { user: string; project: string; slug: string };
+    params: { user: string; project: string; slug: string };
 }) {
-  const slug = decodeURIComponent(params.slug);
-  let mdString;
+    const slug = decodeURIComponent(params.slug);
+    let mdString;
 
-  try {
-    mdString = await api.site.getPageContent.query({
-      gh_username: params.user,
-      projectName: params.project,
-      slug: slug !== "undefined" ? slug.split(",").join("/") : "",
-    });
-  } catch (error) {
-    return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <div className="text-center">
-          <h1 className="mb-4 text-6xl font-bold text-gray-800">404</h1>
-          <p className="mb-8 text-xl text-gray-600">Page Not Found</p>
-          <p className="text-gray-500">
-            The page you are looking for might not exist or has been moved.
-          </p>
-        </div>
-      </div>
+    try {
+        mdString = await api.site.getPageContent.query({
+            gh_username: params.user,
+            projectName: params.project,
+            slug: slug !== "undefined" ? slug.split(",").join("/") : "",
+        });
+    } catch (error) {
+        return (
+            <div className="flex min-h-screen items-center justify-center bg-gray-100">
+                <div className="text-center">
+                    <h1 className="mb-4 text-6xl font-bold text-gray-800">404</h1>
+                    <p className="mb-8 text-xl text-gray-600">Page Not Found</p>
+                    <p className="text-gray-500">
+                        The page you are looking for might not exist or has been moved.
+                    </p>
+                </div>
+            </div>
+        );
+    }
+    if (!mdString) {
+        notFound();
+    }
+    const permalinks =
+        (await api.site.getSitePermalinks.query({
+            gh_username: params.user,
+            projectName: params.project,
+        })) ?? [];
+
+    const { mdxSource, frontMatter } = await parse(
+        mdString,
+        "mdx",
+        {},
+        permalinks,
     );
-  }
-  if (!mdString) {
-    notFound();
-  }
-  const permalinks =
-    (await api.site.getSitePermalinks.query({
-      gh_username: params.user,
-      projectName: params.project,
-    })) ?? [];
 
-  const { mdxSource, frontMatter } = await parse(
-    mdString,
-    "mdx",
-    {},
-    permalinks,
-  );
+    // TODO temporary solution for fetching files from github
+    const { gh_repository, gh_branch } = (await api.site.get.query({
+        gh_username: params.user,
+        projectName: params.project,
+    }))!;
 
-  // TODO temporary solution for fetching files from github
-  const { gh_repository, gh_branch } = (await api.site.get.query({
-    gh_username: params.user,
-    projectName: params.project,
-  }))!;
-
-  return (
-    <>
-      <MdxPage
-        source={mdxSource}
-        frontMatter={frontMatter}
-        gh_repository={gh_repository}
-        gh_branch={gh_branch}
-      />
-    </>
-  );
+    return (
+        <>
+            <MdxPage
+                source={mdxSource}
+                frontMatter={frontMatter}
+                gh_repository={gh_repository}
+                gh_branch={gh_branch}
+            />
+        </>
+    );
 }
