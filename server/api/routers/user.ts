5cceaee5dfed194a6cf72ee448b9bc41754490b6
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-02-27 15:27:43 +0100

    [github lib,user router][s]: cleanup & refactoring

--- server/api/routers/user.ts
+++ server/api/routers/user.ts
@@ -1,12 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { githubFetch } from "@/lib/github";
-import type {
-  GitHubScope,
-  GitHubAPIOrganization,
-  GitHubAPIRepository,
-  GitHubRepository,
-} from "@/lib/github";
+import { fetchGitHubScopes, fetchGitHubScopeRepositories } from "@/lib/github";
 
 export const userRouter = createTRPCRouter({
   getSites: protectedProcedure
@@ -17,91 +11,20 @@ export const userRouter = createTRPCRouter({
         take: input?.limit,
       });
     }),
-  getGitHubScopes: protectedProcedure.query<GitHubScope[]>(async ({ ctx }) => {
+  getGitHubScopes: protectedProcedure.query(async ({ ctx }) => {
     const accessToken = ctx.session.accessToken;
-
-    // Fetching organizations the user belongs to.
-    // https://docs.github.com/en/rest/orgs/members?apiVersion=2022-11-28#list-organization-memberships-for-the-authenticated-user
-    const orgsResponse = await githubFetch({
-      url: `/user/orgs`,
-      accessToken,
-      cacheOptions: {
-        cache: "no-store",
-      },
-    });
-    const orgs = (await orgsResponse.json()) as GitHubAPIOrganization[];
-
-    // Fetching the user's own account information.
-    // https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-the-authenticated-user
-    const userResponse = await githubFetch({
-      url: `/user`,
-      accessToken,
-      cacheOptions: {
-        cache: "no-store",
-      },
-    });
-    const user = await userResponse.json();
-
-    // Combining both orgs and the user's GitHub login as selectable scopes.
-    return [
-      { login: user.login, type: "User", avatar_url: user.avatar_url },
-    ].concat(
-      orgs.map((org) => ({
-        login: org.login,
-        type: "Organization",
-        avatar_url: org.avatar_url,
-      })),
-    ) as GitHubScope[];
+    return await fetchGitHubScopes(accessToken);
   }),
   getGitHubScopeRepos: protectedProcedure
     .input(z.string())
-    .query<GitHubRepository[]>(async ({ ctx, input: scope }) => {
-      const accessToken = ctx.session.accessToken;
-      let page = 1;
-      let allRepos: GitHubRepository[] = [];
-      let hasNextPage = false;
-      let scopeReposUrl: string;
-
-      let urlParams = new URLSearchParams({
-        per_page: "100",
+    .query(async ({ ctx, input: scope }) => {
+      const {
+        accessToken,
+        user: { username },
+      } = ctx.session;
+      return await fetchGitHubScopeRepositories({
+        scope: username === scope ? "self" : scope,
+        accessToken,
       });
-
-      if (ctx.session.user.username !== scope) {
-        // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-organization-repositories
-        scopeReposUrl = `/orgs/${scope}/repos`;
-        // urlParams.set('type', 'public');
-      } else {
-        // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
-        scopeReposUrl = `/user/repos`;
-        urlParams.set("affiliation", "owner,collaborator");
-      }
-      do {
-        urlParams.set("page", `${page}`);
-        const response = await githubFetch({
-          url: `${scopeReposUrl}?${urlParams.toString()}`,
-          accessToken,
-          cacheOptions: {
-            cache: "no-store",
-          },
-        });
-        const repos = (await response.json()) as GitHubAPIRepository[];
-        allRepos = allRepos.concat(
-          repos.map((repo) => ({
-            id: repo.id,
-            name: repo.name,
-            full_name: repo.full_name,
-            owner: repo.owner.login,
-          })),
-        );
-
-        const linkHeader = response.headers.get("link");
-        hasNextPage = linkHeader ? linkHeader.includes('rel="next"') : false;
-        page++;
-      } while (hasNextPage);
-
-      // sort alphabetically by name
-      allRepos.sort((a, b) => a.name.localeCompare(b.name));
-
-      return allRepos;
     }),
 });
