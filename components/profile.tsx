742ffea4870dd25787906b83032865e23544f0aa
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-03-05 23:11:04 +0100

    [dashboard][xs]: disable profile settings button

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,6 +1,6 @@
 import { getSession } from "@/server/auth";
 import { redirect } from "next/navigation";
-import Link from "next/link";
+/* import Link from "next/link"; */
 import Image from "next/image";
 import LogoutButton from "./logout-button";
 
@@ -12,7 +12,7 @@ export default async function Profile() {
 
   return (
     <div className="flex w-full items-center justify-between">
-      <Link
+      {/* <Link
         href="/settings"
         className="flex w-full flex-1 items-center space-x-3 rounded-lg px-2 py-1.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
       >
@@ -29,7 +29,22 @@ export default async function Profile() {
         <span className="truncate text-sm font-medium">
           {session.user.name}
         </span>
-      </Link>
+      </Link> */}
+      <div className="flex w-full flex-1 items-center space-x-3 rounded-lg px-2 py-1.5 dark:text-white">
+        <Image
+          src={
+            session.user.image ??
+            `https://avatar.vercel.sh/${session.user.email}`
+          }
+          width={40}
+          height={40}
+          alt={session.user.name ?? "User avatar"}
+          className="h-6 w-6 rounded-full"
+        />
+        <span className="cursor-default truncate text-sm font-medium">
+          {session.user.name}
+        </span>
+      </div>
       <LogoutButton />
     </div>
   );
