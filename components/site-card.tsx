f50519eb71d6ac6fbf90ccc1bdb6f406cab9a1f8
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-03-05 11:14:05 +0100

    [dashboard][xs]: simplify site card url on /sites page

--- components/site-card.tsx
+++ components/site-card.tsx
@@ -11,12 +11,14 @@ export default function SiteCard({
   data: Site;
   username: string;
 }) {
-  const url =
+  const href =
     env.NEXT_PUBLIC_VERCEL_ENV === "production"
       ? `https://dev.${env.NEXT_PUBLIC_ROOT_DOMAIN}/@${username}/${data.projectName}`
       : env.NEXT_PUBLIC_VERCEL_ENV === "preview"
         ? `https://staging-dev.${env.NEXT_PUBLIC_ROOT_DOMAIN}/@${username}/${data.projectName}`
         : `http://dev.${env.NEXT_PUBLIC_ROOT_DOMAIN}/@${username}/${data.projectName}`;
+
+  const url = new URL(href).pathname.replace(/^\/+/, "");
 
   return (
     <div className="relative rounded-lg border border-stone-200 pb-10 shadow-md transition-all hover:shadow-xl dark:border-stone-700 dark:hover:border-white">
@@ -52,7 +54,7 @@ export default function SiteCard({
       </Link>
       <div className="absolute bottom-4 flex w-full justify-between space-x-4 px-4">
         <a
-          href={url}
+          href={href}
           target="_blank"
           rel="noreferrer"
           className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
