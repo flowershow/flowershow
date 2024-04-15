3f4dac2af2e5b2cb3cd421990b1a444f38167f39
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-04-15 15:08:11 +0200

    [cloud nav,#328][xs]: change Visit button color; add link to docs

--- app/cloud/(dashboard)/site/[id]/settings/header.tsx
+++ app/cloud/(dashboard)/site/[id]/settings/header.tsx
@@ -84,23 +84,22 @@ export default async function SiteSettingsHeader({
             </button>
           </a>
         </span>
+        <span className="ml-3">
+          <SyncSiteButton />
+        </span>
         <span className="ml-3 block">
           <a href={url} target="_blank" rel="noreferrer">
             <button
               type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              className="inline-flex items-center rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
             >
               <ArrowTopRightOnSquareIcon
-                className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
+                className="-ml-0.5 mr-1.5 h-5 w-5"
                 aria-hidden="true"
               />
               Visit
             </button>
           </a>
-        </span>
-
-        <span className="ml-3">
-          <SyncSiteButton />
         </span>
       </div>
     </div>
