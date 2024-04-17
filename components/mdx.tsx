6ecdf9097b17fb1820f9e4a5e95ad102543cb228
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-04-17 20:07:44 +0200

    replace duplicate article tag with div

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -27,13 +27,13 @@ const MDX: React.FC<MDXProps> = ({ source, metadata }) => {
   };
 
   return (
-    <article id="mdxpage" suppressHydrationWarning>
+    <div id="mdxpage" suppressHydrationWarning>
       <Layout>
         <ErrorBoundary FallbackComponent={MDXFallbackComponent}>
           <MDXRemote {...source} components={components} />
         </ErrorBoundary>
       </Layout>
-    </article>
+    </div>
   );
 };
 
