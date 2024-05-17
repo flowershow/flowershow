5d8169d22e2bcfd5899a3b6b9c07d965f6362ae0
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-05-17 14:44:21 +0200

    site config type improvements

--- const/config.ts
+++ const/config.ts
@@ -1,4 +1,6 @@
-const config = {
+import { ExtendedSiteConfig } from "@/components/types";
+
+const config: ExtendedSiteConfig = {
   title: "DataHub",
   description:
     "Turn your markdown into a website in a couple of clicks. Avoid the hassle and complexity of deploying yourself.",
@@ -12,7 +14,7 @@ const config = {
     url: "https://datahub.io/",
     logo: "/datahub-cube.svg",
   },
-  github: "datopian/datahub",
+  github: "datopian/datahub", // public repo
   discord: "https://discord.com/invite/KrRzMKU",
   analytics: "G-R6X92HM43Q",
   footerLinks: [
@@ -21,13 +23,11 @@ const config = {
       subItems: [
         { href: "/", name: "Home" },
         { href: "/docs/terms-of-service", name: "Terms of Service" },
-        // { href: "/docs/privacy", name: "Privacy Policy" },
         { href: "/blog", name: "Blog" },
         { href: "/docs", name: "Docs" },
         { href: "/collections", name: "Collections" },
         { href: "/opensource", name: "Open Source" },
         { href: "/enterprise", name: "Enterprise" },
-        // { href: "https://old.datahub.io/", name: "Old DataHub" },
       ],
     },
     {
@@ -36,9 +36,8 @@ const config = {
         { href: "https://discord.com/invite/KrRzMKU", name: "Chat" },
         {
           name: "Discussions",
-          href: "https://github.com/datahubio/datahub/discussions",
+          href: "https://github.com/datopian/datahub/discussions",
         },
-        // { href: "/docs/faq", name: "FAQ" },
       ],
     },
     {
@@ -56,9 +55,6 @@ const config = {
     { name: "ENTERPRISE", href: "/enterprise" },
     { name: "FORUM", href: "https://github.com/datahubio/datahub/discussions" },
     { name: "CHAT", href: "https://discord.com/invite/KrRzMKU" },
-    // { name: "FIND DATA", href: "/search" },
-    // { name: "TOOLS", href: "/download" },
-    // { name: "TOOLKIT", href: "/toolkit" },
   ],
   social: [
     { label: "discord", href: "https://discord.com/invite/KrRzMKU" },
@@ -66,6 +62,6 @@ const config = {
     { label: "twitter", href: "https://twitter.com/datopian" },
     { label: "github", href: "https://github.com/datopian" },
   ],
-} as any;
+};
 
 export default config;
