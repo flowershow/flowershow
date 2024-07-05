a8f18dbfd6ed077d31a39da6c86ba8adc178391f
Daniellappv <45828069+Daniellappv@users.noreply.github.com>
2024-07-05 14:48:16 +0300

    Update Showcase - adding more showcase examples.tsx

--- app/home/_components/Showcase.tsx
+++ app/home/_components/Showcase.tsx
@@ -31,6 +31,61 @@ const showcaseItems = [
     description: "Dataset ( Trends in Atmospheric Carbon Dioxide )",
     href: "https://datahub.io/core/co2-ppm",
   },
+  {
+    image: "/datahub-showcase-bitcoin-coingecko.png",
+    description: "3,468 days of Bitcoin history",
+    href: "https://datahub.io/@cheredia19/bitcoin-coingecko",
+  },
+  {
+    image: "/datahub-showcase-50-yt-channel-most-subscribers.png",
+    description: "50 YouTube channels most subscribers",
+    href: "https://datahub.io/@cheredia19/50-yt-channels-most-subscribers",
+  },
+  {
+    image: "/datahub-showcase-yoy-food-prices.png",
+    description: "YoY Food prices",
+    href: "https://datahub.io/@cheredia19/yoy-food-prices",
+  },
+  {
+    image: "/datahub-showcase-happiness-report.png",
+    description: "World Happiness Report 2024",
+    href: "https://datahub.io/@cheredia19/happiness2024",
+  },
+  {
+    image: "/datahub-showcase-imprisoned-journalists-2024.png",
+    description: "Imprisoned journalists 2024",
+    href: "https://datahub.io/@cheredia19/imprisoned-journalists-2024",
+  },
+  {
+    image: "/datahub-showcase-press-freedom-2024.png",
+    description: "Press Freedom 2024",
+    href: "https://datahub.io/@cheredia19/press-freedom-2024",
+  },
+  {
+    image: "/datahub-showcase-cross-section-scattering.png",
+    description: "Cross Section Scattering",
+    href: "https://datahub.io/@LuisVCSilva/cross_section_scattering",
+  },
+  {
+    image: "/datahub-showcase-tourist-arrivals-in-nepal.png",
+    description: "Tourist arrivals in Nepal",
+    href: "https://datahub.io/@sagargg/tourist-arrivals-in-nepal",
+  },
+  {
+    image: "/datahub-showcase-city-ranking-kz-2022.png",
+    description: "City Ranking KZ 2022",
+    href: "https://datahub.io/@TyuninaA/city-ranking",
+  },
+  {
+    image: "/datahub-showcase-notes-lifeitself.png",
+    description: "Lifeitself notes",
+    href: "https://notes.lifeitself.org/",
+  },
+  {
+    image: "/datahub-showcase-david-handbook.png",
+    description: "David's handbook",
+    href: "https://datahub.io/@davidgasquez/handbook",
+  },
 ];
 
 const Showcase: React.FC = () => {
