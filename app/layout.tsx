717b7b0dc5fbf71d46d0660d3ebf60c06c11483b
Yedige Ashmet <55581804+gradedSystem@users.noreply.github.com>
2024-07-12 15:56:24 +0500

    [#1197,fix][xs] Making final fixes - refs #1197

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata = {
     description: config.description,
     images: [
       {
-        url: config.logo,
+        url: "/thumbnail.png",
         width: 1200,
         height: 627,
         alt: "Thumbnail",
@@ -34,7 +34,7 @@ export const metadata = {
     description: config.description,
     images: [
       {
-        url: config.logo,
+        url: "/thumbnail.png",
         width: 800,
         height: 418,
         alt: "Thumbnail",
@@ -58,25 +58,6 @@ export default function RootLayout({
           integrity="sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn"
           crossOrigin="anonymous"
         />
-        <meta name="description" content={config.description} />
-
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content={config.title} />
-        <meta property="og:description" content={config.description} />
-        <meta property="og:image" content={config.logo} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="627" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="DataHub" />
-
-        {/* Twitter Card Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={config.title} />
-        <meta name="twitter:description" content={config.description} />
-        <meta name="twitter:image" content={config.logo} />
-        <meta name="twitter:image:width" content="800" />
-        <meta name="twitter:image:height" content="418" />
-        <meta name="twitter:creator" content="@datopian" />
       </head>
       <body className={cn(cal.variable, inter.variable, "bg-background")}>
         <TRPCReactProvider headers={headers()}>
