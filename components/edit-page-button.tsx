64e9e30f5479f11ea5327564f9c563c3629a366a
William Lima <willy.lima89@gmail.com>
2024-07-30 11:04:45 -0100

    update edit-page-button

--- components/edit-page-button.tsx
+++ components/edit-page-button.tsx
@@ -2,6 +2,7 @@
 import { EditIcon } from "lucide-react";
 import { Button } from "./button";
 import { useSiteContext } from "./site/provider";
+import Link from "next/link";
 
 export default function EditPageButton({
   pageContentPath,
@@ -12,15 +13,17 @@ export default function EditPageButton({
 
   return (
     config?.showEditLink && (
-      <div className="mx-auto px-6 pt-4 lg:px-8 ">
-        <Button
-          variant="outline"
+      <div
+        className="mx-auto px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32"
+        data-testid="edit-page-btn"
+      >
+        <Link
           href={`https://github.com/${settings?.repository}/edit/${settings?.branch}/${pageContentPath}`}
           className="mt-4 flex items-center gap-1"
           target="_blank"
         >
           Edit this page <EditIcon width={16} />
-        </Button>
+        </Link>
       </div>
     )
   );
