779ccac2c80fe7e46e5772bf344f9a227f7b93bc
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2024-08-18 17:58:40 +0200

    [fix][s]: correct escaping >, < signs (#514)
    Related to: https://github.com/datopian/datahub/issues/1143

--- lib/process-mdx-string.test.ts
+++ lib/process-mdx-string.test.ts
@@ -2,39 +2,43 @@ import { processMdxString } from "./process-mdx-string";
 import "@testing-library/jest-dom";
 
 describe("processMdxString", () => {
-  it("should replace < with &lt;", () => {
-    const result = processMdxString("<");
-    expect(result).toBe("&lt;");
+  it("should replace 'stanadlone' < with &lt;", () => {
+    const result = processMdxString("abc < xyz");
+    expect(result).toBe("abc &lt; xyz");
   });
 
-  it("should replace > with &gt;", () => {
-    const result = processMdxString(">");
-    expect(result).toBe("&gt;");
-  });
-
-  it("should replace <= with &lt;=", () => {
-    const result = processMdxString("<=");
-    expect(result).toBe("&lt;=");
+  it("should replace 'stanadlone' > with &gt;", () => {
+    const result = processMdxString("abc > xyz");
+    expect(result).toBe("abc &gt; xyz");
   });
 
   it("should replace => with =&gt;", () => {
-    const result = processMdxString("=>");
-    expect(result).toBe("=&gt;");
+    const result = processMdxString("abc => xyz");
+    expect(result).toBe("abc =&gt; xyz");
   });
 
-  it("should replace >= with &gt;=", () => {
-    const result = processMdxString(">=");
-    expect(result).toBe("&gt;=");
-  });
-
-  it("should replace <=> with &lt;=&gt;", () => {
-    const result = processMdxString("<=>");
-    expect(result).toBe("&lt;=&gt;");
+  it("should replace <= with &lt;=", () => {
+    const result = processMdxString("abc <= xyz");
+    expect(result).toBe("abc &lt;= xyz");
   });
 
   it("shouldn't replace < and > in JSX elements", () => {
-    const input = `<LineChart data={[]}/>`;
-    const expectedOutput = input;
+    const input = `
+<div className="test"
+     id="test"
+  >
+  <p>abc<br/>xyz</p>
+  <p>abc < xyz</p>
+  <p>abc > xyz</p>
+</div>`;
+    const expectedOutput = `
+<div className="test"
+     id="test"
+  >
+  <p>abc<br/>xyz</p>
+  <p>abc &lt; xyz</p>
+  <p>abc &gt; xyz</p>
+</div>`;
     const result = processMdxString(input);
     expect(result).toBe(expectedOutput);
   });
