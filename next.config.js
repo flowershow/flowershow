17a8121c814a862c84841ac44dfaa15e4a01b05d
Ola Rubaj <ola@yway.pl>
2025-02-09 14:57:36 +0100

    fix: add remotePatterns for Next.js Image components

--- next.config.js
+++ next.config.js
@@ -18,15 +18,8 @@ module.exports = {
   },
   images: {
     remotePatterns: [
-      { hostname: "public.blob.vercel-storage.com" },
-      { hostname: "res.cloudinary.com" },
-      { hostname: "abs.twimg.com" },
-      { hostname: "pbs.twimg.com" },
-      { hostname: "avatar.vercel.sh" },
-      { hostname: "avatars.githubusercontent.com" },
-      { hostname: "www.google.com" },
-      { hostname: "flag.vercel.app" },
-      { hostname: "illustrations.popsy.co" },
+      { hostname: "*.datahub.io" },
+      { hostname: "*.flowershow.app" },
       { hostname: "localhost" },
     ],
   },
