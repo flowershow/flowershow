6aa5524a016dbc30b17b8d788e4ac4da6ece10d2
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-03-18 15:05:41 +0100

    fix: rm margin from Edit this page link

--- components/edit-page-button.tsx
+++ components/edit-page-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function EditPageButton({ url }: { url: string }) {
   return (
-    <div className="mx-auto px-6 py-2 md:py-4" data-testid="edit-page-btn">
+    <div className="py-2 md:py-4" data-testid="edit-page-btn">
       <Link
         href={url}
         className="mt-4 flex w-fit items-center gap-1 font-normal no-underline hover:underline"
