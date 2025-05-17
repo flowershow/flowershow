9b0fa5c3504eb25e3d076d2eef36d9577a06f676
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-05-17 23:09:56 +0200

    fix: support Tailwind 4 in MDX

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,9 @@
 // uno.config.ts
 import { defineConfig } from "unocss";
-import presetWind from "@unocss/preset-wind3";
+import presetWind4 from "@unocss/preset-wind4";
 
 export default defineConfig({
-  presets: [presetWind()],
+  presets: [presetWind4()],
   include: [
     "app/**/*.{js,jsx,ts,tsx,md,mdx}",
     "components/**/*.{js,jsx,ts,tsx,md,mdx}",
