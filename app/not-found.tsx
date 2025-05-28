a4b21552f90a922ffc64fdc7606e5c832be6234b
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-05-28 16:44:15 +0200

    fix: add link to 404 debugging doc page to 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,6 +7,18 @@ export default function NotFoundPage() {
         <p className="text-gray-500">
           The page you are looking for might not exist or has been moved.
         </p>
+        <p className="mt-4 text-sm text-gray-400">
+          Site creator? You can find debugging instructions{" "}
+          <a
+            href="https://flowershow.app/blog/how-to-debug-404-pages#common-causes-of-404-errors"
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
       </div>
     </div>
   );
