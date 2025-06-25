d25cac01c8b53bd068f76b3e03d6f310ad246856
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-06-25 16:10:02 +0200

    chore: adjust scripts

--- scripts/fetch-app-config.mjs
+++ scripts/fetch-app-config.mjs
@@ -8,10 +8,10 @@ import "dotenv/config";
       console.error("APP_CONFIG_URL is not defined in environment variables.");
       process.exit(1);
     }
-    if (fs.existsSync("./config.json")) {
-      console.log("Product configuration already exists.");
-      process.exit(0);
-    }
+    // if (fs.existsSync("./config.json")) {
+    //   console.log("Product configuration already exists.");
+    //   process.exit(0);
+    // }
     const res = await axios.get(process.env.APP_CONFIG_URL);
     const productConfig = res.data;
     fs.writeFileSync("./config.json", JSON.stringify(productConfig, null, 2));
