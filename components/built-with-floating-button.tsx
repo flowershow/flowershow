7b5e5d3cf9d37a888b6673b45c70459d17e1a870
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-08-01 02:11:39 +0200

    fix: make it harder to override built-with floating button styles

--- components/built-with-floating-button.tsx
+++ components/built-with-floating-button.tsx
@@ -6,20 +6,23 @@ const config = getConfig();
 
 const buttonStyles: React.CSSProperties = {
   position: "fixed",
-  bottom: 24,
-  right: 32,
-  zIndex: 100,
-  display: "flex",
-  alignItems: "center",
-  backgroundColor: "white",
-  padding: "0.5rem 0.75rem",
-  borderRadius: 40,
-  fontSize: "0.875rem",
-  fontWeight: 500,
-  fontFamily: "var(--font-inter)",
-  color: "#18181B",
-  letterSpacing: "-0.025em",
-  boxShadow: "0 8px 20px 0 rgba(0,0,0,.08)",
+  bottom: "24 !important",
+  right: "32 !important",
+  zIndex: "100 !important",
+  display: "flex !important",
+  alignItems: "center !important",
+  backgroundColor: "white !important",
+  padding: "0.5rem 0.75rem !important",
+  borderRadius: "40 !important",
+  boxShadow: "0 8px 20px 0 rgba(0,0,0,.08) !important",
+};
+
+const spanStyles: React.CSSProperties = {
+  fontSize: "0.875rem !important",
+  fontWeight: "500 !important",
+  fontFamily: "var(--font-inter) !important",
+  color: "#18181B !important",
+  letterSpacing: "-0.025em !important",
 };
 
 const logoStyles = {
@@ -33,7 +36,7 @@ export default function BuiltWithFloatingButton() {
       href={config.landingPageUrl}
       style={buttonStyles}
     >
-      <span>Built with </span>
+      <span style={spanStyles}>Built with </span>
       <Image
         width={18}
         height={18}
@@ -41,7 +44,7 @@ export default function BuiltWithFloatingButton() {
         src={config.logo}
         alt="Logo"
       />
-      <span>{config.title}</span>
+      <span style={spanStyles}>{config.title}</span>
     </Link>
   );
 }
