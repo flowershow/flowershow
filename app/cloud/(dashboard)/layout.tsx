fb75c8822c97f0703fe5c368916e5167cc402985
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-08-14 00:32:35 +0200

    fix: dashboard fonts

--- app/cloud/(dashboard)/layout.tsx
+++ app/cloud/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/dashboard/footer";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="grid min-h-[100dvh] grid-rows-[auto_1fr_auto]">
+    <div className="grid min-h-[100dvh] grid-rows-[auto_1fr_auto] font-dashboard-body">
       <Nav>
         <Suspense fallback={<div>Loading...</div>}>
           <Profile />
