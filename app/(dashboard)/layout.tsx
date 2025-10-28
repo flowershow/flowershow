3b2459e605a35db8128bae6d881b8b88d02c8507
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2025-10-28 19:04:48 +0100

    chore: use latest katext styles

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -72,8 +72,8 @@ export default async function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css"
-          integrity="sha384-GvrOXuhMATgEsSwCs4smul74iXGOixntILdUW9XmUC6+HX0sLNAK3q71HotJqlAn"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.25/dist/katex.min.css"
+          integrity="sha384-WcoG4HRXMzYzfCgiyfrySxx90XSl2rxY5mnVY5TwtWE6KLrArNKn0T/mOgNL0Mmi"
           crossOrigin="anonymous"
         />
       </head>
