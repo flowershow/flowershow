c054aaaad4908eb43071e1bcea2234a7ac458b75
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2026-01-23 13:25:40 +0100

    chore: clean up expired sites

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { serve } from 'inngest/next';
 import { inngest } from '@/inngest/client';
-import { deleteSite, syncSite } from '@/inngest/functions';
+import { cleanupExpiredSites, deleteSite, syncSite } from '@/inngest/functions';
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncSite, deleteSite],
+  functions: [syncSite, deleteSite, cleanupExpiredSites],
 });
