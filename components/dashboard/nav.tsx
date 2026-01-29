e47c07d28629a4406dc91b06ab977b490643da04
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2026-01-29 23:22:20 +0100

    feat: add link to /tokens to profile dropdown menu

--- components/dashboard/nav.tsx
+++ components/dashboard/nav.tsx
@@ -141,6 +141,14 @@ export default function Nav({ children }: { children: ReactNode }) {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
               >
                 <MenuItem>
+                  <Link
+                    href="/tokens"
+                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                  >
+                    API Tokens
+                  </Link>
+                </MenuItem>
+                <MenuItem>
                   <button
                     onClick={() => signOut()}
                     className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
