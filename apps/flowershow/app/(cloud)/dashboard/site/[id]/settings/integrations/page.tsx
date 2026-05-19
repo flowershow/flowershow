e5bbc39beb0b9d40b93b654167d6e6b63abed791
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2026-05-19 21:31:33 +0200

    rm integrations page

--- apps/flowershow/app/(cloud)/dashboard/site/[id]/settings/integrations/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default async function IntegrationsRedirect(props: {
-  params: Promise<{ id: string }>;
-}) {
-  const params = await props.params;
-  redirect(`/site/${decodeURIComponent(params.id)}/settings/github`);
-}
